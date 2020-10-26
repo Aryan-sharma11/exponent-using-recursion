7a20cc6cc4af2f9251a64cf30d27ee9713aac43d
Aryan-sharma11 <54109867+Aryan-sharma11@users.noreply.github.com>
2020-10-26 20:33:02 +0530

    Update power.java
    named meaning full variable names

--- power.java
+++ power.java
@@ -1,25 +1,32 @@
-
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
 public class power {
-    public static int pow(int n,int p)
+    public static int pow(int num,int p)
     {
         if(p==0)
             return 1;
         if(p%2==0)
-            return  pow(n*n,p/2);
+            return  pow(num*num,p/2);
         else
-            return n*pow(n*n,(p-1)/2);
+            return num*pow(num*num(p-1)/2);
     }
     public static void main(String[] args) {
-            int n;
-            int p,res;
-            System.out.println("enter the number");
-            Scanner sc= new Scanner(System.in);
-            n=sc.nextInt();
-            System.out.println("enter the power");
-            p=sc.nextInt();
-            res=pow(n,p);
-            System.out.println(res);
+        int num;
+        int p,result;
+        System.out.println("enter the number");
+        Scanner sc= new Scanner(System.in);
+        num=sc.nextInt();
+        System.out.println("enter the power");
+        p=sc.nextInt();
+        result=pow(num,p);
+        System.out.println(result);
 
     }
 }
